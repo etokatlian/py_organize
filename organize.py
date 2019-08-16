93ddca1fee1048fea1b6b25fd781cb275e0d3a20
eric-techfabric <eric@techfabric.co>
2019-08-16 14:12:22 -0700

    watches downloads dir

--- organize.py
+++ organize.py
@@ -7,20 +7,38 @@ import time
 
 
 class Handler(FileSystemEventHandler):
+
     def on_modified(self, event):
-        for filename in os.listdir(folder_to_track):
-            print(folder_to_track)
-            print(filename)
-            src = folder_to_track + "/" + filename
-            new_destination = folder_destination + "/" + filename
-            os.rename(src, new_destination)
+
+        extDict = {
+            '.pdf':     0,
+            '.jpeg':    0,
+            '.png':     0,
+            '.html':    0,
+            '.doc':     0,
+            '.txt':     0,
+            '.dmg':     0,
+            '.docx':    0,
+        }
+
+        for filename in os.listdir(source_dir):
+            split = os.path.splitext(filename)
+            extension = split[1]
+            dotSpliced = extension[1:]
+
+            if extension in extDict:
+                dest = "/Users/erictokatlian/Desktop/organized_downloads/" + dotSpliced
+                src = source_dir + "/" + filename
+                new_dest = dest + "/" + filename
+                os.rename(src, new_dest)
+            else:
+                pass
 
 
-folder_to_track = "/Users/erictokatlian/Desktop/myFolder"
-folder_destination = "/Users/erictokatlian/Desktop/newFolder"
+source_dir = "/Users/erictokatlian/Downloads"
 event_handler = Handler()
 observer = Observer()
-observer.schedule(event_handler, folder_to_track, recursive=True)
+observer.schedule(event_handler, source_dir, recursive=True)
 observer.start()
 
 try:
---
+++ readme.md
@@ -0,0 +1,8 @@
+automated file organization
+---
+
+### My downloads folder is a mess. This python script listens for changes in my filesystem's download dir and automatically organizes all incoming downloads into categorized dirs on my desktop. 
+
+#### i.e.: 
+- I download a .txt file from some website. Instead of cluttering up my downloads dir, it gets organized into `/Users/erictokatlian/organized_downloads/txt`
+- Next I download some pdf files, all of those will get organized into `/Users/erictokatlian/organized_downloads/pdf`
